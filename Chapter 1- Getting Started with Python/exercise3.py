8c3d367334a698f757776212d171f64be4af0944
ummeerr <124669645+ummeerr@users.noreply.github.com>
2023-03-08 12:41:46 +0400

    ch1 last 3 exercise.

--- Chapter 1- Getting Started with Python/exercise3.py
+++ Chapter 1- Getting Started with Python/exercise3.py
@@ -1,4 +1,4 @@
 import datetime
 now = datetime.datetime.now()
-print("Currnt date and time : ")
+print("Currnt date and time:")
 print(now.strftime("%Y-%M-%D %H-%M-%S"))
---
+++ Chapter 1- Getting Started with Python/exercise4.py
@@ -0,0 +1,4 @@
+x = "Nissan"
+y = "Toyota"
+z = "Honda"
+print(x, y, z)
